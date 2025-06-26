1366f1dc1f8a372ac1e859e1c9c69bb5c9c6f810
cfmcfmcfm <conrad.mahnkopf@wearelean.com>
2025-06-26 17:05:26 +0200

    modified patterns

--- src/components/molecules/StatsSection.tsx
+++ src/components/molecules/StatsSection.tsx
@@ -13,7 +13,6 @@ type SubItem = {
 type StatsItemProps = {
   title: string;
   subtitle: string;
-
   textColor?: "white" | undefined;
   subItems: SubItem[];
 };
@@ -26,7 +25,6 @@ const statsItems: StatsItemProps[] = [
   {
     title: "Pitch Decks Received",
     subtitle: "The breakdown of pitch decks that we've received",
-
     textColor: "white",
     subItems: [
       { from: 0, to: 222 },
@@ -39,7 +37,6 @@ const statsItems: StatsItemProps[] = [
     title: "Meetings Held",
     subtitle:
       "Of pitch decks reviewed, this is a breakdown of those that progressed to meetings with us",
-
     subItems: [
       { from: 0, to: 87 },
       { from: 0, to: 32, prefix: "All Women", suffix: "%" },
@@ -51,7 +48,6 @@ const statsItems: StatsItemProps[] = [
     title: "All Venture",
     subtitle:
       "We've been tracking equity of our dealflow since February 2024, all other data is all-time, since venture inception. This is a breakdown of our investments across all venture by count.",
-
     subItems: [
       { from: 0, to: 90 },
       { from: 0, to: 13, prefix: "All Women", suffix: "%" },
@@ -62,7 +58,6 @@ const statsItems: StatsItemProps[] = [
   {
     title: "Startup Investments",
     subtitle: "This is a breakdown of our startups by amount invested.",
-
     subItems: [
       { from: 0, to: 67 },
       { from: 0, to: 15, prefix: "All Women", suffix: "%" },
@@ -73,7 +68,6 @@ const statsItems: StatsItemProps[] = [
   {
     title: "Fund Investments",
     subtitle: "This is a breakdown of funds by amount invested.",
-
     subItems: [
       { from: 0, to: 23 },
       { from: 0, to: 13, prefix: "All Women", suffix: "%" },
@@ -83,16 +77,17 @@ const statsItems: StatsItemProps[] = [
   },
 ];
 
-const HATCH_PATTERN_MAP: Record<string, string[]> = {
-  default: ["bg-hatch-diagonal", "bg-hatch-cross", "bg-hatch-dots"],
-};
-
-const getHatchPatterns = (pattern: string) =>
-  HATCH_PATTERN_MAP[pattern] || HATCH_PATTERN_MAP["default"];
+// 1. mainItem = cross
+// 2. subItems = [dots, diagonal-one, diagonal-two]
+const HATCH_PATTERN_LIST = [
+  "bg-hatch-dots",
+  "bg-hatch-diagonal",
+  "bg-hatch-cross",
+  "bg-hatch-lines",
+];
 
 const RemainingStatsItems = ({ items }: RemainingStatsItemProps) => {
-  const hatchList = getHatchPatterns("default");
-  const subItemPatterns = items.map((_, i) => hatchList[i % hatchList.length]);
+  const subItemPatterns = items.map((_, i) => HATCH_PATTERN_LIST[i + 1] ?? "");
 
   return (
     <div className="w-full flex text-4xl">
@@ -100,15 +95,18 @@ const RemainingStatsItems = ({ items }: RemainingStatsItemProps) => {
         <div
           key={i}
           className={cn(
-            "flex flex-col items-start justify-end p-3 min-h-24",
+            "flex flex-col items-start justify-end p-3",
             subItemPatterns[i]
           )}
           style={{ width: `${item.to}%` }}
         >
-          {item.prefix && <span className="text-base">{item.prefix}</span>}
-          <div>
-            <CountUp from={+item.from} to={+item.to} />
-            {item.suffix && <span>{item.suffix}</span>}
+          <div className="bg-white leading-none">
+            {item.prefix && <span className="text-base">{item.prefix}</span>}
+
+            <div>
+              <CountUp from={+item.from} to={+item.to} />
+              {item.suffix && <span>{item.suffix}</span>}
+            </div>
           </div>
         </div>
       ))}
@@ -116,20 +114,20 @@ const RemainingStatsItems = ({ items }: RemainingStatsItemProps) => {
   );
 };
 
-const StatsItem = ({ title, subtitle, pattern, subItems }: StatsItemProps) => {
+const StatsItem = ({ title, subtitle, subItems }: StatsItemProps) => {
   const mainItem = subItems[0];
   const otherItems = subItems.slice(1);
-  const mainPattern = getHatchPatterns(pattern)[0];
+
+  const mainPattern = HATCH_PATTERN_LIST[0]; // "bg-hatch-cross"
 
   return (
-    <li className="flex w-full group hover:bg-gray-200 transition-colors duration-300">
+    <li className="flex w-full bg-white group hover:bg-gray-200 transition-colors duration-300">
       <div className="flex flex-col p-4 min-h-52 gap-2 max-w-md w-full">
         <h2 className="text-4xl">{title}</h2>
         <p>{subtitle}</p>
       </div>
 
       <div className={cn("flex-1 flex flex-col justify-end")}>
-        {/* MAIN ITEM */}
         {mainItem && (
           <div
             className={cn(
@@ -140,17 +138,14 @@ const StatsItem = ({ title, subtitle, pattern, subItems }: StatsItemProps) => {
             {mainItem.prefix && (
               <span className="text-base">{mainItem.prefix}</span>
             )}
-            <div>
+            <div className="bg-white">
               <CountUp from={+mainItem.from} to={+mainItem.to} />
               {mainItem.suffix && <span>{mainItem.suffix}</span>}
             </div>
           </div>
         )}
 
-        {/* REMAINING ITEMS */}
-        {otherItems.length > 0 && (
-          <RemainingStatsItems items={otherItems} pattern={pattern} />
-        )}
+        {otherItems.length > 0 && <RemainingStatsItems items={otherItems} />}
       </div>
     </li>
   );
@@ -163,7 +158,7 @@ const StatsSection = () => {
         Key Performance Indicators
       </h2>
 
-      <div className="w-full">
+      <div className="w-full flex flex-col">
         {statsItems.map((item, i) => (
           <StatsItem {...item} key={i} />
         ))}
