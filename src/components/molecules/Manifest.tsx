70b0985a92161f8a14620a051285a056b33e6e59
cfmcfmcfm <conrad.mahnkopf@wearelean.com>
2025-06-30 14:43:26 +0200

    modified manifest

--- src/components/molecules/Manifest.tsx
+++ src/components/molecules/Manifest.tsx
@@ -86,60 +86,58 @@ const Manifest = () => {
   }, []);
 
   return (
-    <section
-      id="manifest"
-      ref={container}
-      className="text-4xl flex relative w-full"
-    >
-      {/* LEFT PINNED INDEX */}
-      <div className="large-number-container w-1/2 flex justify-start items-start h-screen px-8 py-16">
-        <div className="flex space-x-2 text-[16vw] font-bold text-red leading-none">
-          {[0, 1, 2].map((i) => (
-            <div key={i} className="overflow-y-hidden h-[1em] tracking-tight">
-              <div ref={setDigitRef(i)} className="flex flex-col">
-                {digits.map((d) => (
-                  <div className="inline" key={d}>
-                    {d}
-                  </div>
-                ))}
+    <section id="manifest" className="py-24 bg-red">
+      <div ref={container} className="text-4xl flex relative w-full">
+        {/* LEFT PINNED INDEX */}
+        <div className="large-number-container w-1/2 flex justify-start items-start h-screen px-8 py-16">
+          <div className="flex text-[16vw] font-bold text-black leading-none">
+            {[0, 1, 2].map((i) => (
+              <div key={i} className="overflow-y-hidden h-[1em]">
+                <div ref={setDigitRef(i)} className="flex flex-col">
+                  {digits.map((d) => (
+                    <div className="inline" key={d}>
+                      {d}
+                    </div>
+                  ))}
+                </div>
               </div>
-            </div>
-          ))}
-        </div>
-      </div>
-
-      {/* Invisible triggers for scroll tracking */}
-      <div className="w-0">
-        <div className="h-[50vh]"></div>
-        {manifestItems.map((_, i) => (
-          <div
-            key={i}
-            className="index-trigger h-[50vh] w-full pointer-events-none"
-          />
-        ))}
-        <div className="h-[50vh]"></div>
-      </div>
-
-      {/* RIGHT PINNED MANIFEST TEXTS */}
-      <div className="manifest-right w-1/2 h-screen flex justify-end items-end p-4">
-        <div>
-          <h2 className="whitespace-nowrap uppercase text-base text-red">
-            Manifest
-          </h2>
-
-          <p className="max-w-xl text-pretty indent-24">
-            {manifestItems.map((item, i) => (
-              <span
-                key={i}
-                className={cn(
-                  "transition-opacity duration-300",
-                  i === activeIndex ? "text-black" : "text-black/20"
-                )}
-              >
-                {item + " "}
-              </span>
             ))}
-          </p>
+          </div>
+        </div>
+
+        {/* Invisible triggers for scroll tracking */}
+        <div className="w-0">
+          <div className="h-[50vh]"></div>
+          {manifestItems.map((_, i) => (
+            <div
+              key={i}
+              className="index-trigger h-[50vh] w-full pointer-events-none"
+            />
+          ))}
+          <div className="h-[50vh]"></div>
+        </div>
+
+        {/* RIGHT PINNED MANIFEST TEXTS */}
+        <div className="manifest-right w-1/2 h-screen flex justify-end items-end p-4">
+          <div>
+            <h2 className="whitespace-nowrap uppercase text-base text-black">
+              Manifest
+            </h2>
+
+            <p className="max-w-xl text-pretty indent-24">
+              {manifestItems.map((item, i) => (
+                <span
+                  key={i}
+                  className={cn(
+                    "transition-opacity duration-300",
+                    i === activeIndex ? "text-white" : "text-white/20"
+                  )}
+                >
+                  {item + " "}
+                </span>
+              ))}
+            </p>
+          </div>
         </div>
       </div>
     </section>
