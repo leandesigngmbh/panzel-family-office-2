0232c01dd286163e19457acb7761e937ddafec82
cfmcfmcfm <conrad.mahnkopf@wearelean.com>
2025-07-03 14:14:36 +0200

    cleaned pin test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,45 +1,7 @@
-import NavColorSwitcher from "@/components/atoms/NavColorSwitcher";
-import Contact from "@/components/molecules/Contact";
-import Hero from "@/components/molecules/Hero";
-import Intro from "@/components/molecules/Intro";
-import Manifest from "@/components/molecules/Manifest";
 import PinTest from "@/components/molecules/PinTest";
-import Priciples from "@/components/molecules/Priciples";
-import StatsSection from "@/components/molecules/StatsSection";
-import Team from "@/components/molecules/Team";
-import Footer from "@/components/organisms/Footer";
 
 const Home = () => {
-  return (
-    <PinTest />
-    // <>
-    //   {/* <NavColorSwitcher id="hero" light={true}>
-    //     <Hero />
-    //   </NavColorSwitcher> */}
-
-    //   {/* <PinTest /> */}
-
-    //   <main className="bg-white relative">
-    //     <Intro />
-
-    //     {/* <PinTest /> */}
-
-    //     <Priciples />
-
-    //     <NavColorSwitcher id="manifest" light={true}>
-    //       <Manifest />
-    //     </NavColorSwitcher>
-
-    //     <StatsSection />
-
-    //     <Team />
-
-    //     <Contact />
-    //   </main>
-
-    //   <Footer />
-    // </>
-  );
+  return <PinTest />;
 };
 
 export default Home;
