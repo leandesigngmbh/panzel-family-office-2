072072d499a600173a901f56707f4167dae11c9d
cfmcfmcfm <conrad.mahnkopf@wearelean.com>
2025-07-07 15:48:53 +0200

    modified contact

--- src/components/molecules/Contact.tsx
+++ src/components/molecules/Contact.tsx
@@ -4,53 +4,55 @@ const Contact = () => {
   return (
     <section
       id="contact"
-      className="w-full md:px-4 p-2 py-24 grid gap-8 md:gap-0 md:grid-cols-3 bg-black/5"
+      className="w-full flex bg-neutral-100 md:px-4 px-2 py-24 flex-col gap-8"
     >
-      <div className="flex flex-col w-full pr-8">
-        <h2 className="whitespace-nowrap uppercase text-base text-red">
-          Contact
-        </h2>
+      <div className="flex flex-col gap-8 max-w-2/3 mx-auto">
+        <div className="flex flex-col w-full pr-8">
+          <h2 className="whitespace-nowrap uppercase text-base text-red">
+            Contact
+          </h2>
 
-        <p className="text-4xl max-w-xl indent-24">
-          If you have questions or would like to learn more about our services
-          and how Panzel Family Office can work with you, contact us
-          confidentially here.
-        </p>
+          <p className="text-4xl max-w-xl indent-24">
+            If you have questions or would like to learn more about our services
+            and how Panzel Family Office can work with you, contact us
+            confidentially here.
+          </p>
+        </div>
+
+        <form action="#" className="space-y-8 w-full col-span-2 pt-3">
+          <div>
+            <input
+              type="email"
+              id="email"
+              className="border-b focus:ring-0 placeholder-black focus:outline-none focus:border-black block w-full py-2.5"
+              placeholder="Email"
+              required
+            />
+          </div>
+
+          <div>
+            <input
+              type="text"
+              id="subject"
+              className="block py-2.5 w-full border-b focus:ring-0 placeholder-black focus:outline-none focus:border-black"
+              placeholder="Subject"
+              required
+            />
+          </div>
+
+          <div className="sm:col-span-2">
+            <textarea
+              id="message"
+              className="block min-h-40 py-2.5 w-full border-b focus:ring-0 placeholder-black focus:outline-none focus:border-black"
+              placeholder="Message"
+            ></textarea>
+          </div>
+
+          <Button size="lg" variant="default" type="submit">
+            Send message
+          </Button>
+        </form>
       </div>
-
-      <form action="#" className="space-y-8 w-full col-span-2 pt-3">
-        <div>
-          <input
-            type="email"
-            id="email"
-            className="border-b focus:ring-0 placeholder-black focus:outline-none focus:border-black block w-full py-2.5"
-            placeholder="Email"
-            required
-          />
-        </div>
-
-        <div>
-          <input
-            type="text"
-            id="subject"
-            className="block py-2.5 w-full border-b focus:ring-0 placeholder-black focus:outline-none focus:border-black"
-            placeholder="Subject"
-            required
-          />
-        </div>
-
-        <div className="sm:col-span-2">
-          <textarea
-            id="message"
-            className="block min-h-40 py-2.5 w-full border-b focus:ring-0 placeholder-black focus:outline-none focus:border-black"
-            placeholder="Message"
-          ></textarea>
-        </div>
-
-        <Button size="lg" variant="default" type="submit">
-          Send message
-        </Button>
-      </form>
     </section>
   );
 };
