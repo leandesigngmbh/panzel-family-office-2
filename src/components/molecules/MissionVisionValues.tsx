ab0ce506b2b9e573d210cf6239fdc9ec2a778dd7
cfmcfmcfm <conrad.mahnkopf@wearelean.com>
2025-07-07 15:03:23 +0200

    modified ui

--- src/components/molecules/MissionVisionValues.tsx
+++ src/components/molecules/MissionVisionValues.tsx
@@ -16,7 +16,7 @@ const MissionVisionValues = () => {
   ];
 
   return (
-    <section className="py-24 md:px-4 px-2 bg-black text-white">
+    <section className="py-24 md:px-4 px-2 bg-black text-white -mt-2">
       <div className="grid grid-cols-2 max-w-7xl mx-auto border border-current rounded-2xl">
         <div className="p-24 flex flex-col gap-2 border-e border-current">
           <h2 className="text-4xl">Mission</h2>
