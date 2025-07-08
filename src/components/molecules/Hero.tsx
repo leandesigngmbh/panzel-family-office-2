ec1dac67579acb9fb77f95f02069d61cbdc5ed3b
cfmcfmcfm <conrad.mahnkopf@wearelean.com>
2025-07-08 16:34:41 +0200

    performance test

--- src/components/molecules/Hero.tsx
+++ src/components/molecules/Hero.tsx
@@ -7,8 +7,8 @@ const Hero = () => {
       <div data-speed="0.2" className="h-full">
         <Spline
           style={{ height: "100%" }}
-          scene="https://prod.spline.design/vmkVRuLus8-blXQe/scene.splinecode"
-          // scene="https://prod.spline.design/Z7246DtFSIej6I4W/scene.splinecode"
+          // scene="https://prod.spline.design/vmkVRuLus8-blXQe/scene.splinecode"
+          scene="https://prod.spline.design/Z7246DtFSIej6I4W/scene.splinecode"
         />
       </div>
     </section>
