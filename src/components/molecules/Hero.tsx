3a530835f4df825836b59e68167c96d8fb772222
cfmcfmcfm <conrad.mahnkopf@wearelean.com>
2025-07-09 11:12:52 +0200

    modified hero

--- src/components/molecules/Hero.tsx
+++ src/components/molecules/Hero.tsx
@@ -2,9 +2,9 @@ import Spline from "@splinetool/react-spline/next";
 
 const Hero = () => {
   return (
-    <section className="h-screen sm:h-[150vh]">
+    <section className="h-screen">
       {/* Content */}
-      <div data-speed="0.2" className="h-full">
+      <div data-speed={0.2} className="h-full">
         <Spline
           style={{ height: "100%" }}
           // scene="https://prod.spline.design/vmkVRuLus8-blXQe/scene.splinecode"
