2113b475d97065c462fefcc47ba3e165f36d675f
cfmcfmcfm <conrad.mahnkopf@wearelean.com>
2025-07-09 11:18:28 +0200

    performance test

--- src/components/molecules/Hero.tsx
+++ src/components/molecules/Hero.tsx
@@ -8,7 +8,8 @@ const Hero = () => {
         <Spline
           style={{ height: "100%" }}
           // scene="https://prod.spline.design/vmkVRuLus8-blXQe/scene.splinecode"
-          scene="https://prod.spline.design/Z7246DtFSIej6I4W/scene.splinecode"
+          // scene="https://prod.spline.design/Z7246DtFSIej6I4W/scene.splinecode"
+          scene="https://prod.spline.design/4N3ASol9FwuSRvMF/scene.splinecode"
         />
       </div>
     </section>
