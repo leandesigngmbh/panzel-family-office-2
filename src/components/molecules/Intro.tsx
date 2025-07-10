65dabe2fd36f3bd1cc8ecb4bac58877c35968345
cfmcfmcfm <conrad.mahnkopf@wearelean.com>
2025-07-10 14:26:08 +0200

    added image to scene

--- src/components/molecules/Intro.tsx
+++ src/components/molecules/Intro.tsx
@@ -1,3 +1,5 @@
+import Image from "next/image";
+
 const Intro = () => {
   return (
     <section className="py-24 md:px-4 px-2 text-2xl md:text-6xl flex flex-col gap-6">
@@ -6,7 +8,13 @@ const Intro = () => {
         cultural momentum.
       </p>
 
-      <div className="aspect-video w-full bg-gray-200 flex items-center justify-center">
+      <div className="aspect-video w-full bg-gray-200 flex items-center justify-center relative overflow-hidden">
+        <Image
+          // data-speed="0.75"
+          fill
+          src="/CZ25W12IX0010_rgb_comp_01.png"
+          alt="dasda"
+        />
         Video
       </div>
     </section>
